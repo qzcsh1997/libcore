cd17bbcd41b5645e7610ac1b04ac6fbdadbb3abc
wdl <wdl@qq.com>
2019-08-13 17:04:50 +0800

    通过ContentProvider获取全局Context
    通过ContentProvider获取全局Context

--- Libcore/app/src/main/java/com/wdl/libcore/App.java
+++ Libcore/app/src/main/java/com/wdl/libcore/App.java
@@ -3,17 +3,19 @@ package com.wdl.libcore;
 import android.app.Application;
 import android.content.Context;
 
-public class App extends Application {
+public class App extends Application
+{
     public static Context mContext;
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
         mContext = this.getApplicationContext();
-        WLibrary.set(this.getApplicationContext());
     }
 
-    public static Context get() {
+    public static Context get()
+    {
         return mContext;
     }
 }
